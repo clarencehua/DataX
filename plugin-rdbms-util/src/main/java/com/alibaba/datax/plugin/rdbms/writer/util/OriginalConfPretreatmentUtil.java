e859afb9878671c794434e42633d82e9e7acff9e
huage <2521859276@qq.com>
2026-06-01 08:08:02 +0000

    pg支持数组形式读取jdbcurl

--- plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/writer/util/OriginalConfPretreatmentUtil.java
+++ plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/writer/util/OriginalConfPretreatmentUtil.java
@@ -58,10 +58,11 @@ public final class OriginalConfPretreatmentUtil {
         for (int i = 0, len = connections.size(); i < len; i++) {
             Configuration connConf = Configuration.from(connections.get(i).toString());
 
-            String jdbcUrl = connConf.getString(Key.JDBC_URL);
-            if (StringUtils.isBlank(jdbcUrl)) {
+            List<String> jdbcUrls = connConf.getList(Key.JDBC_URL, String.class);
+            if (null == jdbcUrls || jdbcUrls.isEmpty()) {
                 throw DataXException.asDataXException(DBUtilErrorCode.REQUIRED_VALUE, "您未配置的写入数据库表的 jdbcUrl.");
             }
+            String jdbcUrl = jdbcUrls.get(0);
 
             jdbcUrl = DATABASE_TYPE.appendJDBCSuffixForWriter(jdbcUrl);
             originalConfig.set(String.format("%s[%d].%s", Constant.CONN_MARK, i, Key.JDBC_URL),
